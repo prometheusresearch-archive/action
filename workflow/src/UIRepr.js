8e0435f99a28a70f84327758f873da6880bc216f
Andrey Popp <8mayday@gmail.com>
2018-03-24 18:39:33 +0300

    workflow: do not use ES2015 in JS

--- workflow/src/UIRepr.js
+++ workflow/src/UIRepr.js
@@ -1,13 +1,11 @@
-module.exports.UIRepr = class UI {
-
-  constructor(name, args, typ, value, query) {
-    this.name = name;
-    this.args = args;
-    this.typ = typ;
-    this.query = query;
-    this.value = value;
-  }
-  inspect() {
-    return `UI(${this.name})`;
-  }
+module.exports.UIRepr = function UI(name, args, typ, value, query) {
+  this.name = name;
+  this.args = args;
+  this.typ = typ;
+  this.query = query;
+  this.value = value;
 }
+
+UI.prototype.inspect = function() {
+  return 'UI(' + this.name + ')';
+};
